3378951e592fd3016ba39727702523f8d7655c50
Cody Heiser <codyheiser49@gmail.com>
2018-04-04 12:10:34 -0500

    split statistical functions and plotting functions out into separate files

---
+++ stats.r
@@ -0,0 +1,121 @@
+#========================= GGPLOT2 CONFIG =========================#
+# The following functions are for preferences and common settings  
+#   for use with ggplot2 visualization package
+
+# set up your workspace
+require('outliers')
+
+# linear model for regressions
+lm_eqn <- function(df,x,y){
+  m <- lm(y ~ x, df);
+  eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
+                   list(a = format(coef(m)[1], digits = 2), 
+                        b = format(coef(m)[2], digits = 2), 
+                        r2 = format(summary(m)$r.squared, digits = 3)))
+  as.character(as.expression(eq));                 
+}
+
+# normalize each value in df to the median for its row. return molten dataframe.
+row.norm <- function(matrix, id.vars=c('Sample'), strtegy = ''){
+  # matrix = dataframe to normalize values in
+  # id.vars = vector of column names to ignore when normalizing
+  # strtegy = transformation to perform on normalized fraction. 'log2', 'log', 'log10', etc. Default is none.
+  
+  # calculate the median for each row for all columns
+  matrix$row.median <- apply(matrix[-which(names(matrix) %in% id.vars)], # ignore any ID variables and normalize over desired values only
+                             FUN = function(x){median(as.numeric(x), na.rm = T)},
+                             MARGIN = 1)
+  melt <- melt(matrix, id.vars = c(unlist(id.vars), 'row.median')) # get values into one column
+  melt$norm <- eval(parse(text = paste0('unlist(',strtegy,'(melt$value/melt$row.median))'))) # calculate normalized value for each column based on row median
+  return(melt)
+}
+
+# normalize each value in df to the global median. return molten dataframe.
+global.norm <- function(matrix, id.vars=c('Sample'), strtegy = ''){
+  # matrix = dataframe to normalize values in
+  # id.vars = vector of column names to ignore when normalizing
+  # strtegy = transformation to perform on normalized fraction. 'log2', 'log', 'log10', etc. Default is none.
+  
+  melt <- melt(matrix, id.vars = id.vars) # get values into one column
+  global.median <- median(melt$value, na.rm = T) # calculate the median for all rows and all columns
+  melt$norm <- eval(parse(text = paste0('unlist(',strtegy,'(melt$value/global.median))'))) # calculate normalized value for each column based on global median
+  return(melt)
+}
+
+# test for normality of a dataset
+norm.test <- function(v, qq = TRUE){
+  # uses Shaprio-Wilk test to determine if a vector of data is normally distributed
+  # log2 and log10-transforms data to see if those are normally distributed as well
+  # v = vector of numerical data to test for normal distribution
+  # qq = output a quantile-quantile plot against the normal distribution?
+  
+  results <- data.frame(transformation = c('Original','Log2-Transformed','Log10-Transformed'),shapiro.results = c('Undet.','Undet.','Undet.'),
+                        shapiro.pval = c(shapiro.test(v)$p.value, 
+                                         try_default(shapiro.test(log2(v))$p.value,default = NA,quiet = T), 
+                                         try_default(shapiro.test(log10(v))$p.value,default = NA,quiet = T))
+                        )
+  # determine if these distributions and transformed distributions are normal
+  results$shapiro.results <- unlist(lapply(results$shapiro.pval, FUN = function(x){
+    if(is.na(x)){return('Not Real')}
+    if(x < 0.05){return('Not Normal')} 
+    if(x >= 0.05){return('Normal')}
+    }))
+  # output q-q plot if necessary
+  if(isTRUE(qq)){
+    qqnorm(v)
+    qqline(v)
+  }
+  # print results to console
+  for(x in 1:nrow(results)){
+    print(paste0(results$transformation[x],' distribution is ',results$shapiro.results[x],', with a p-value of ', signif(results$shapiro.pval[x], 5)))
+  }
+  return(results)
+}
+
+# test for and remove outliers in a dataset
+outlier.test <- function(v){
+  # uses Grubbs test to iteratively identify outliers in a vector of data
+  # returns a mask (logical vector) of values to keep
+  # v = vector of numerical data to test for outliers
+  options(digits = 20) # prevent rounding during as.numeric
+  
+  vhold <- v # hold on to a copy of full v for masking on
+  ref <- 1 # initialize reference for checking vector length. start at 1 to get into loop
+  norm <- 1 # initialize normalizer for output vector
+  norm.mask <- rep(T, length(v)) # initialize logical vector of TRUEs for output
+  
+  while(length(v) != ref){
+    # do the test
+    grubb <- grubbs.test(v)
+    
+    if(grubb$p.value >= 0.05){
+      # if there are no outliers, print message to console and return vector as-is
+      
+      ref <- length(v) # reference becomes full vector length
+      print('There are no outliers in the dataset')
+      
+    }else{
+      # if an outlier is detected, identify it, remove from vector, and loop
+      
+      ref <- length(v) # reference becomes full vector length before you remove an outlier
+      v.mask <- round(v,10) != round(as.numeric(strsplit(grubb$alternative, '\\ ')[[1]][3]),10) # determine position where outlier is in current vector
+      vhold.mask <- round(vhold,10) != round(as.numeric(strsplit(grubb$alternative, '\\ ')[[1]][3]),10) # determine position where outlier is in original vector
+      norm.mask <- norm.mask + vhold.mask 
+      print(paste0(grubb$alternative, ' at position ', which(vhold.mask == FALSE), ' in the vector')) # prints outlier value to console
+      
+      v <- v[v.mask] # remove outlier from vector
+      # check to make sure you didn't mess the vector up
+      if(length(v) < ref - 1){
+        print('Warning: Removed more than one element with the same outlier value')
+      }
+      
+      norm <- norm + 1 # iterate normalizer for output vector
+    }
+  }
+  
+  norm.mask <- norm.mask/norm # normalize aggregate logical vector to number of samples removed. TRUE + TRUE = 2 / 2 (for 1 loop) = 1 = TRUE 
+  norm.mask[norm.mask != 1] <- 0
+  norm.mask <- as.logical(norm.mask)
+  return(norm.mask) # return the logical vector
+  
+}
--- utilityfunctions.r
+++ utilityfunctions.r
@@ -1,21 +1,23 @@
 #========================= CODY FUNCTIONS =========================#
 # The following functions are for general use in loading, reshaping, 
-#   visualizing, and exporting datasets.
+#   manipulating, and exporting datasets.
 
-# set up your workspace
+# Set up your workspace:
+# general packages
 require('reshape2')
 require('plyr')
 require('dplyr')
 require('stringr')
 require('magrittr')
+require('tidyr')
 require('jsonlite')
 require('tools')
 require('readxl')
-require('ggplot2')
-require('gplots')
-require('plotly')
-require('outliers')
-require('here')
+# other source files
+source('stats.r')
+source('ggplot_config.r')
+# working directory tracking using 'here'
+# require('here')
 
 # read in .csv or .xlsx file
 read.default <- function(file, ...){
@@ -92,15 +94,6 @@ read.from <- function(file, start.str, end.str = NA, offset = 0, ...){
   return(df)
 }
 
-# save ggplot as .png image
-to.png <- function(plt, destination = 'plt.png', w = 8, h = 8, r = 700){
-  # plt = ggplot object
-  # destination = filepath, name, and extension of output
-  # w, h = width, height in inches
-  # r = resolution
-  ggsave(filename = destination, plot = plt, width = w, height = h, units = 'in', dpi = r)
-}
-
 # convert POSIXct date-time object to total time in minutes, ignoring date
 to.minutes <- function(x){
   # takes a POSIXct or POSIXt object and converts it to time in minutes
@@ -168,179 +161,3 @@ enumerate <- function(df, col){
   }
   return(unlist(reps))
 }
-
-# normalize each value in df to the median for its row. return molten dataframe.
-row.norm <- function(matrix, id.vars=c('Sample'), strtegy = ''){
-  # matrix = dataframe to normalize values in
-  # id.vars = vector of column names to ignore when normalizing
-  # strtegy = transformation to perform on normalized fraction. 'log2', 'log', 'log10', etc. Default is none.
-  
-  # calculate the median for each row for all columns
-  matrix$row.median <- apply(matrix[-which(names(matrix) %in% id.vars)], # ignore any ID variables and normalize over desired values only
-                             FUN = function(x){median(as.numeric(x), na.rm = T)},
-                             MARGIN = 1)
-  melt <- melt(matrix, id.vars = c(unlist(id.vars), 'row.median')) # get values into one column
-  melt$norm <- eval(parse(text = paste0('unlist(',strtegy,'(melt$value/melt$row.median))'))) # calculate normalized value for each column based on row median
-  return(melt)
-}
-
-# normalize each value in df to the global median. return molten dataframe.
-global.norm <- function(matrix, id.vars=c('Sample'), strtegy = ''){
-  # matrix = dataframe to normalize values in
-  # id.vars = vector of column names to ignore when normalizing
-  # strtegy = transformation to perform on normalized fraction. 'log2', 'log', 'log10', etc. Default is none.
-  
-  melt <- melt(matrix, id.vars = id.vars) # get values into one column
-  global.median <- median(melt$value, na.rm = T) # calculate the median for all rows and all columns
-  melt$norm <- eval(parse(text = paste0('unlist(',strtegy,'(melt$value/global.median))'))) # calculate normalized value for each column based on global median
-  return(melt)
-}
-
-# test for normality of a dataset
-norm.test <- function(v, qq = TRUE){
-  # uses Shaprio-Wilk test to determine if a vector of data is normally distributed
-  # log2 and log10-transforms data to see if those are normally distributed as well
-  # v = vector of numerical data to test for normal distribution
-  # qq = output a quantile-quantile plot against the normal distribution?
-  
-  results <- data.frame(transformation = c('Original','Log2-Transformed','Log10-Transformed'),shapiro.results = c('Undet.','Undet.','Undet.'),
-                        shapiro.pval = c(shapiro.test(v)$p.value, 
-                                         try_default(shapiro.test(log2(v))$p.value,default = NA,quiet = T), 
-                                         try_default(shapiro.test(log10(v))$p.value,default = NA,quiet = T))
-                        )
-  # determine if these distributions and transformed distributions are normal
-  results$shapiro.results <- unlist(lapply(results$shapiro.pval, FUN = function(x){
-    if(is.na(x)){return('Not Real')}
-    if(x < 0.05){return('Not Normal')} 
-    if(x >= 0.05){return('Normal')}
-    }))
-  # output q-q plot if necessary
-  if(isTRUE(qq)){
-    qqnorm(v)
-    qqline(v)
-  }
-  # print results to console
-  for(x in 1:nrow(results)){
-    print(paste0(results$transformation[x],' distribution is ',results$shapiro.results[x],', with a p-value of ', signif(results$shapiro.pval[x], 5)))
-  }
-  return(results)
-}
-
-# test for and remove outliers in a dataset
-outlier.test <- function(v){
-  # uses Grubbs test to iteratively identify outliers in a vector of data
-  # returns a mask (logical vector) of values to keep
-  # v = vector of numerical data to test for outliers
-  options(digits = 20) # prevent rounding during as.numeric
-  
-  vhold <- v # hold on to a copy of full v for masking on
-  ref <- 1 # initialize reference for checking vector length. start at 1 to get into loop
-  norm <- 1 # initialize normalizer for output vector
-  norm.mask <- rep(T, length(v)) # initialize logical vector of TRUEs for output
-  
-  while(length(v) != ref){
-    # do the test
-    grubb <- grubbs.test(v)
-    
-    if(grubb$p.value >= 0.05){
-      # if there are no outliers, print message to console and return vector as-is
-      
-      ref <- length(v) # reference becomes full vector length
-      print('There are no outliers in the dataset')
-      
-    }else{
-      # if an outlier is detected, identify it, remove from vector, and loop
-      
-      ref <- length(v) # reference becomes full vector length before you remove an outlier
-      v.mask <- round(v,10) != round(as.numeric(strsplit(grubb$alternative, '\\ ')[[1]][3]),10) # determine position where outlier is in current vector
-      vhold.mask <- round(vhold,10) != round(as.numeric(strsplit(grubb$alternative, '\\ ')[[1]][3]),10) # determine position where outlier is in original vector
-      norm.mask <- norm.mask + vhold.mask 
-      print(paste0(grubb$alternative, ' at position ', which(vhold.mask == FALSE), ' in the vector')) # prints outlier value to console
-      
-      v <- v[v.mask] # remove outlier from vector
-      # check to make sure you didn't mess the vector up
-      if(length(v) < ref - 1){
-        print('Warning: Removed more than one element with the same outlier value')
-      }
-      
-      norm <- norm + 1 # iterate normalizer for output vector
-    }
-  }
-  
-  norm.mask <- norm.mask/norm # normalize aggregate logical vector to number of samples removed. TRUE + TRUE = 2 / 2 (for 1 loop) = 1 = TRUE 
-  norm.mask[norm.mask != 1] <- 0
-  norm.mask <- as.logical(norm.mask)
-  return(norm.mask) # return the logical vector
-  
-}
-
-# linear model for regressions
-lm_eqn <- function(df,x,y){
-  m <- lm(y ~ x, df);
-  eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
-                   list(a = format(coef(m)[1], digits = 2), 
-                        b = format(coef(m)[2], digits = 2), 
-                        r2 = format(summary(m)$r.squared, digits = 3)))
-  as.character(as.expression(eq));                 
-}
-
-# plot multiple complete plot objects on one image
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL){
-  # Multiple plot function
-  # ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
-  # - cols:   Number of columns in layout
-  # - layout: A matrix specifying the layout. If present, 'cols' is ignored.
-  # If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
-  # then plot 1 will go in the upper left, 2 will go in the upper right, and
-  # 3 will go all the way across the bottom.
-  library(grid)
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
-  numPlots = length(plots)
-  # If layout is NULL, then use 'cols' to determine layout
-  if(is.null(layout)){
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                     ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-  if(numPlots==1){
-    print(plots[[1]])
-  }else{
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    # Make each plot, in the correct location
-    for(i in 1:numPlots){
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))
-    }}}
-
-# color scale
-asgn_colors <- list("primblue"="#4298cc",
-                    "primorange"="#ee7624",
-                    "primgray"="#435563",
-                    "green"="#32d339",
-                    "yellow"="#cedc00",
-                    "red"="#e1261c",
-                    "purple"="#74538F",
-                    "teal"="#20cbd4",
-                    "blue"="#3f97b5",
-                    "lightgray"="#c8c9c7",
-                    "darkgray"="#54585a")
-
-# preferred plotting options
-# call these by adding them (+) to a ggplot object
-plot.opts <- list(
-  theme_bw(),
-  theme(text = element_text(colour = asgn_colors$darkgray),
-        legend.text=element_text(size=9),
-        axis.line = element_line(colour = asgn_colors$darkgray),
-        axis.title=element_text(size=12),
-        axis.text.x=element_text(size=10),
-        axis.text.y=element_text(size=10),
-        plot.title=element_text(size=18))
-)
